161cbb404457a4ba61c5c7af289fdeab76418dd3
sharoon <kpsharoon7@gmail.com>
2026-04-28 01:08:46 +0500

    added MVC production grade folder structure

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./src/config/db";
+
+
+import cookieparser from "cookie-parser";
+
+
+import authRoutes from "./src/routes/auth/authRoutes.js";
+
+app.use(cookieparser());
+
+
 dotenv.config({
   path: "./.env",
 });
@@ -13,6 +23,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+
+// register api routes here
+app.use("/api/users",authRoutes)
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const userSchema  = mongoose.Schema({
     name:{
@@ -18,4 +20,43 @@ const userSchema  = mongoose.Schema({
 }  ,{timestamps: true}
 )
 
+userSchema.pre('save', async function (next){
+    if(!this.isModified("password")) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+})
+
+userSchema.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            id: this._id,
+            name: this.name,
+            email: this.email
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRE
+        }
+    )
+
+}
+userSchema.methods.generateRefreshToken = function(){
+   return jwt.sign(
+    {
+        id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRE
+    }
+   )
+}
+
 export const User = mongoose.model("User", userSchema)
+
